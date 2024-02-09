a2d5a0684cc88d1a8f15d5e22d0c8f82146e8a1d
ryounosuke-t <ryo0tomo330@gmail.com>
2024-02-09 13:39:50 +0900

    add favorite function

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,8 @@
-import { User } from "@prisma/client";
+import { User, Listing } from "@prisma/client";
+
+export type safeListing = Omit<Listing, "createdAt"> & {
+  createdAt: string;
+};
 
 export type SafeUser = Omit<
   User,
